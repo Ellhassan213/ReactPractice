8521b7ac85033ffa673e9ca02fd7852a6a60a4b9
Lawal <ellhassan213@gmail.com>
2021-10-02 23:08:38 +0100

    Added some column structure to checkout page

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -22,53 +22,57 @@ function Checkout() {
     return (
         <main className="checkout-page">
             <h1>Checkout Page</h1>
-            <hr />
-            <OrderSummary />
-            <hr />
-            <h2>Delivery</h2>
-            <form onSubmit={placeOrder}>
-                <input
-                    type="text"
-                    name="firstName" 
-                    value={formInputs.firstName}
-                    onChange={handleChange} 
-                    placeholder="First Name" 
-                />
-                <br />
-                <input
-                    type="text"
-                    name="lastName" 
-                    value={formInputs.lastName}
-                    onChange={handleChange} 
-                    placeholder="Last Name" 
-                />
-                <br />
-                <input
-                    type="text"
-                    name="email" 
-                    value={formInputs.email}
-                    onChange={handleChange} 
-                    placeholder="Email" 
-                />
-                <br />
-                <input
-                    type="text"
-                    name="phone" 
-                    value={formInputs.phone}
-                    onChange={handleChange} 
-                    placeholder="Phone Number" 
-                />
-                <br />
-                <textarea
-                    type="text"
-                    name="address"
-                    value={formInputs.address}
-                    onChange={handleChange}
-                    placeholder="Address"
-                />
-                <br />
-                {<button onClick={placeOrder}>{buttonText}</button>}
-            </form>
+            <div className="columns">
+                <div>
+                    <h2>Delivery</h2>
+                    <form onSubmit={placeOrder}>
+                        <input
+                            type="text"
+                            name="firstName" 
+                            value={formInputs.firstName}
+                            onChange={handleChange} 
+                            placeholder="First Name" 
+                        />
+                        <br />
+                        <input
+                            type="text"
+                            name="lastName" 
+                            value={formInputs.lastName}
+                            onChange={handleChange} 
+                            placeholder="Last Name" 
+                        />
+                        <br />
+                        <input
+                            type="text"
+                            name="email" 
+                            value={formInputs.email}
+                            onChange={handleChange} 
+                            placeholder="Email" 
+                        />
+                        <br />
+                        <input
+                            type="text"
+                            name="phone" 
+                            value={formInputs.phone}
+                            onChange={handleChange} 
+                            placeholder="Phone Number" 
+                        />
+                        <br />
+                        <textarea
+                            type="text"
+                            name="address"
+                            value={formInputs.address}
+                            onChange={handleChange}
+                            placeholder="Address"
+                        />
+                        <br />
+                        {<button onClick={placeOrder}>{buttonText}</button>}
+                    </form>
+                </div>
+                <div className="order-summary">
+                    <OrderSummary />
+                </div>
+            </div>
         </main>
     )
 }
